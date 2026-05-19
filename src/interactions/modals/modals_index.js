98ea65c420110af39269298c4249863cd13b200e
dammish <arvidliljefors00@gmail.com>
2026-05-19 11:50:03 +0200

    Update and rename ticket.js to modals_index.js

---
+++ src/interactions/modals/modals_index.js
@@ -0,0 +1,7 @@
+import {
+  createTicketModalHandler,
+  closeTicketModalHandler,
+} from '../../handlers/ticketButtons.js';
+import { vouchSubmitHandler } from '../../handlers/vouchButtons.js';
+
+export default [createTicketModalHandler, closeTicketModalHandler, vouchSubmitHandler];
